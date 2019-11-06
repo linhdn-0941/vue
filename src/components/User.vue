b8a900d3cdbb6fb51d693ac4222aca92bdbde695
Duong Ngoc Linh <duong.ngoc.linh@sun-asterisk.com>
2019-11-06 16:16:08 +0700

    Infused data between component

---
+++ src/components/User.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="user">
+        <p>Name: {{ name }}</p>
+        <p>Age: {{ age }} tuoi</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-6">
+                <app-user-detail v-bind:age="age" v-on:ageWasUpdated="resetAge"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-6">
+                <app-user-edit v-bind:age="age" v-on:ageWasUpdated="resetAge"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function () {
+            return {
+                name: 'linhdn',
+                age: 16
+            };
+        },
+        methods: {
+            resetAge(age) {
+                this.age = age
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    .user{
+        background: springgreen;
+        text-align: center;
+        padding: 20px;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="user-detail">
+        <h3>User Details</h3>
+        <p>Age: {{ age }}</p>
+        <button @click="editAge()">Change age</button>
+    </div>
+</template>
+
+<script>
+    import {eventBus} from '../main'
+    export default {
+        props: ['age'],
+        methods: {
+            editAge() {
+                this.age += 1
+                this.$emit('ageWasUpdated', this.age)
+            }
+        },
+        created() {
+            eventBus.$on('ageSentByBus', (age) => {
+                this.age = age
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    .user-detail{
+        padding: 10px;
+        background: red;
+    }
+</style>
